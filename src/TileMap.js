ad6f65deb33b9ba2909cda8c6197bd292d4db092
HoangHa03 <118713422+hahoang03@users.noreply.github.com>
2023-10-22 16:41:19 +0700

    Almost Pacman

--- src/TileMap.js
+++ src/TileMap.js
@@ -1,3 +1,6 @@
+import Pacman from "./Pacman.js"
+import MovingDirection from "./MovingDirection.js"
+
 export default class TileMap{
     constructor(tileSize){
         this.tileSize = tileSize;
@@ -8,18 +11,23 @@ export default class TileMap{
         
         this.wall = new Image();
         this.wall.src = "../images/wall.png";
-    }
+    } 
+
+
+//1 - wall
+//0 - dots
+//4 - pacman
 
     map = [
         [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [ 1, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 1],
+        [ 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1],
+        [ 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+        [ 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+        [ 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1],
+        [ 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+        [ 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+        [ 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1],
         [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
         [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     ];
@@ -58,9 +66,74 @@ export default class TileMap{
         );
     }
 
+    getPacman(velocity){
+        for(let row=0; row <this.map.length; row++){
+            for (let column=0; column <this.map[row].length; column++ ){
+                let tile = this.map[row][column];
+                if(tile === 4){// return Pacman to the user
+                    this.map[row][column] = 0;
+                    return new Pacman(
+                        column * this.tileSize, 
+                        row * this.tileSize, 
+                        this.tileSize, 
+                        velocity,
+                        this);
+                }
+            }
+        }
+    }
+
+
     setCanvasSize(canvas){
         canvas.width = this.map[0].length * this.tileSize;
         canvas.height = this.map.length * this.tileSize;
         
+    }
+
+    didCollideWithEnvironment(x,y,direction){
+
+        if(direction == null ){
+            return;
+        }
+        
+        if(
+            Number.isInteger(x/this.tileSize) &&
+            Number.isInteger(y/this.tileSize)
+        ){
+            let column = 0;
+            let row =0;
+            let nextColumn =0;
+            let nextRow = 0;
+
+
+            switch (direction){
+                case MovingDirection.right:
+                    nextColumn = x + this.tileSize;
+                    column = nextColumn / this.tileSize;
+                    row = y / this.tileSize;
+                    break;
+                 case MovingDirection.left:
+                    nextColumn = x - this.tileSize;
+                    column = nextColumn / this.tileSize;
+                    row = y / this.tileSize;
+                    break;
+                case MovingDirection.up:
+                    nextRow = y - this.tileSize;
+                    row = nextRow / this.tileSize;
+                    column = x/this.tileSize;
+                    break;
+                case MovingDirection.down:
+                    nextRow = y + this.tileSize;
+                    row = nextRow / this.tileSize;
+                    column = x/this.tileSize;
+                    break;
+            }
+
+            const tile = this.map[row][column]
+            if(tile ==1){
+                return true;
+            }
+        }
+        return false;
     }
 }
