b99c840e924811d2b27330ff0d11d1a184f6d68c
HoangHa03 <118713422+hahoang03@users.noreply.github.com>
2023-10-22 18:29:09 +0700

    Update TileMap.js

--- src/TileMap.js
+++ src/TileMap.js
@@ -153,7 +153,9 @@ export default class TileMap{
         if(Number.isInteger(row) && Number.isInteger(column)){
             if(this.map[row][column] === 0){
                 this.map[row][column] = 5;
+                return true;
             }
         }
+        return false;
     }
 }
