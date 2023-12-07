d6c6a05de6f17d31690c0f80c14b404e21d621a1
KenG1M <tpqhenp001@gmail.com>
2023-12-07 20:48:26 +0700

    complete base Enemy.js

--- src/Enemy.js
+++ src/Enemy.js
@@ -1,8 +1,6 @@
 import MovingDirection from "./MovingDirection.js";
 
 export default class Enemy{
-
-
   constructor(x,y,tileSize,velocity,tileMap){
     this.x =x;
     this.y = y;
@@ -10,29 +8,65 @@ export default class Enemy{
     this.tileMap = tileMap;
     this.tileSize = tileSize;
 
-
-
     this.#loadImages();
     this.movingDirection = Math.floor(Math.random() * Object.keys(MovingDirection).length);
 
     this.directionTimerdefault = this.#random(10,50);
     this.directionTimer = this.directionTimerdefault;
 
+    this.scaredAboutToExpireTimerDefault = 10;
+    this.scaredAboutToExpireTimer = this.scaredAboutToExpireTimerDefault;
   }
   draw(ctx){
-    this.#move();
-    this.#changeDirection();
+  //   this.#move();
+  //   this.#changeDirection();
  
-  ctx.drawImage(this.image, this.x, this.y, this.tileSize,this.tileSize);
-
-
-
-
-
-
-
+  // ctx.drawImage(this.image, this.x, this.y, this.tileSize,this.tileSize);
+    if (!pause) {
+      this.#move();
+      this.#changeDirection();
+    }
+    this.#setImage(ctx, pacman);
   }
 
+  collideWith(pacman) {
+    const size = this.tileSize / 2;
+    if (
+      this.x < pacman.x + size &&
+      this.x + size > pacman.x &&
+      this.y < pacman.y + size &&
+      this.y + size > pacman.y
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  #setImage(ctx, pacman) {
+    if (pacman.powerDotActive) {
+      this.#setImageWhenPowerDotIsActive(pacman);
+    } else {
+      this.image = this.normalGhost;
+    }
+    ctx.drawImage(this.image, this.x, this.y, this.tileSize, this.tileSize);
+  }
+
+  #setImageWhenPowerDotIsActive(pacman) {
+    if (pacman.powerDotAboutToExpire) {
+      this.scaredAboutToExpireTimer--;
+      if (this.scaredAboutToExpireTimer === 0) {
+        this.scaredAboutToExpireTimer = this.scaredAboutToExpireTimerDefault;
+        if (this.image === this.scaredGhost) {
+          this.image = this.scaredGhost2;
+        } else {
+          this.image = this.scaredGhost;
+        }
+      }
+    } else {
+      this.image = this.scaredGhost;
+    }
+  }
 
   #changeDirection(){
     this.directionTimer--;
